763fdc9a59109432b685b226de44cdd0ef6c0f0a
Aidan Shark <alirezastar5888@gmail.com>
2025-08-04 10:33:38 +1000

    Continue coding: updated files

--- src/components/sub/Hero.jsx
+++ src/components/sub/Hero.jsx
@@ -3,25 +3,30 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className="h-screen grid place-items-center bg-gray-900 text-white px-7">
+    <div className="h-screen grid place-items-center">
       <div className="text-center space-y-4">
-        <div className="flex flex-col items-center space-y-2">
+        <div className="flex flex-col items-center justify-center gap-y-3 font-light capitalize">
+          <div className="flex items-center justify-center"> 
           <Image 
             src="/LittleGuy.png"
             alt="Personal Image" 
             width={100} 
             height={100} 
-            priority 
+            priority={true}
+            className="h-auto w-[150px]"
           />
-          <span className="text-lg font-medium">Hello there</span>
+          <span className="absolute text-3xl font-semibold text-white">Hello there</span>
         </div>
-        <h1 className="text-3xl font-bold">My name is Aidan Habibi</h1>
-        <p className="text-sm">
+        </div>
+        <h1 className="text-center text-3xl font-bold tracking-wider text-gray-400">My name is Aidan Habibi</h1>
+        <p className="text-lg tracking-wider text-gray-500">
           I am a third-year Computer Science student at the University of Wollongong.
         </p>
         <div className="flex justify-center space-x-6 pt-5">
-          <a href="#" className="text-yellow-400 hover:underline">Icon</a>
-          <a href="#" className="text-yellow-400 hover:underline">Talk to me</a>
+          <a href="#" className="text-yellow-400 hover:underline">
+            Icon</a>
+          <a href="#" className="mx-auto mt-7 block w-max rounded-lg bg-yellow-700 px-3 py-1 font-light capitilize tracking-wider
+          text-white hover:bg-yellow-400 transition-colors">Talk to me</a>
         </div>
       </div>
     </div>
