076d39b3bba8b7a59485d9effe55fcdd99519b54
Aidan Shark <alirezastar5888@gmail.com>
2025-09-01 16:54:08 +1000

    Add Heading.jsx component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 "use client"
 import Hero from '../components/sub/Hero';
+import about from '../components/sub/about';
 
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-black to-gray-900 text-white px-4 text-center">
       <Hero />
+      <about />
     </main>
   );
 }
