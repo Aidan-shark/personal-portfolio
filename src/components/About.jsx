c53350e1696c357acde1069e1496e438d58a7ac5
Aidan Shark <alirezastar5888@gmail.com>
2025-09-04 00:19:22 +1000

    update project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,34 @@
 'use client'
 import Heading from './sub/Heading'
+import Achievement from './sub/Achievement'
+import Image from 'next/image'
 
 const About = () => {
     return (
            <div>
             
-                        <Heading />
+                <Heading />
+           <div>
+            <Image src='/about-me.png' alt="About Image" width={400} height={400} />
+            <div>
+                <span>
+                    Arrow left
+                    <p>
+                        About me
+                    </p>
+                    <a href="/aidan.cv.pdf" download="">
+                    <span>
+                        Download CV
+                    </span>
+                    <span>
+                        Download icon
+                    </span>
+
+                    </a>
+                </span>
+            </div>
+            </div>
+            <div><Achievement /></div>
            </div>
 
             
